3a7597285a9a134713f6a9c0be5d6a7b6fab2875
robbygiovanni <42507991+robbygiovanni@users.noreply.github.com>
2020-07-27 15:45:38 +0700

    a

---
+++ JFstudio/app/Models/ContactModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ContactModel extends Model
+{
+    protected $table = 'contact';
+    protected $primaryKey = 'id_contact';
+
+
+    public function insertMesssage()
+    {
+    }
+}
